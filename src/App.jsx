44d62bf8d68aa750bf71be397cc59d4d7f8c22e2
Rikhiz <fadelhizkil22@gmail.com>
2025-05-21 14:49:01 +0700

    starter project done by ketua

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Routes,Route } from 'react-router-dom'
+import MainLayout from './components/MainLayout'
+import Dashboard from './assets/Pages/Dashboard'
+
+function App() {
+  return (
+
+    <Routes>
+      <Route element={<MainLayout/>}>
+      <Route path="/" element={<Dashboard/>}/>
+      </Route>
+    </Routes>
+  )
+}
+
+export default App

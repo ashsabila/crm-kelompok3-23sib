96e57d08cd9232c40b821db6da7eb461fc9837cf
ashsabila <144917996+ashsabila@users.noreply.github.com>
2025-06-17 21:59:25 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import WhatsappFloatingButton from './assets/Pages/WhatsappFloatingButton';
 import FaqAdmin from './assets/Pages/FaqAdmin';
 import Notifikasi from './assets/Pages/Notifikasi'
 import HelpCenter from './assets/Pages/HelpCenter';
-import Laporan from './assets/Pages/Laporan'
+import Laporan from './assets/Pages/Laporan';
 function App() {
   return (
       <>

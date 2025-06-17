e3dba86a89a8e2d74d945f81a3bdaaa97e1b55a1
ashsabila <aisha23si@mahasiswa.pcr.ac.id>
2025-06-17 22:17:45 +0700

    tesss

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,6 @@ import WhatsappFloatingButton from './assets/Pages/WhatsappFloatingButton';
 import FaqAdmin from './assets/Pages/FaqAdmin';
 import Notifikasi from './assets/Pages/Notifikasi'
 import HelpCenter from './assets/Pages/HelpCenter';
-import Laporan from './assets/Pages/Laporan';;
 import Laporan from './assets/Pages/Laporan';
 import BookingSchedule from './assets/Pages/BookingSchedule';
 
